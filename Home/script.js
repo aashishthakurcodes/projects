e39179272425c1ddc1f8b6983862dbebf3fd6a83
Aashish Thakur <j9183208@gmail.com>
2023-04-05 22:53:44 +0530

    Togglebtn solved

--- Home/script.js
+++ Home/script.js
@@ -1,54 +1,39 @@
-const btn=document.getElementById("navbtn");
-// const ul=document.getElementById("navul");
-
-const newdiv=document.createElement("div");
-newdiv.setAttribute('class','newdivdata')
-
-const ul1=document.createElement("ul");
-ul1.setAttribute("class","ul1")
-
-const l1=document.createElement("li");
-const anchor=document.createElement("a");
-anchor.innerHTML = 'Home';
-anchor.href = '';
-l1.appendChild(anchor)
-
-
-const l2=document.createElement("li");
-const anchor2=document.createElement("a");
-anchor2.innerHTML = 'About';
-anchor2.href = '../About/index.html';
-l1.appendChild(anchor2);
-
-
-const l3=document.createElement("li");
-const anchor3=document.createElement("a");
-anchor3.innerHTML = 'Contact';
-anchor3.href = '../Contact/index.html';
-l1.appendChild(anchor3)
-
-
-ul1.appendChild(l1);
-ul1.appendChild(l2);
-ul1.appendChild(l3);
-
-console.log(ul1);
-
-newdiv.appendChild(ul1);
-console.log(newdiv);
-const toggledata=document.getElementById("toggledata");
-btn.addEventListener('click',function(){
-    if(toggledata.style.display === 'none'){
-        toggledata.appendChild(newdiv)
-        toggledata.style.display ='block';
-        
-    }
-    else{
-        toggledata.style.display ='none';
+const btn = document.getElementById("navbtn");
+const toggledata = document.getElementById("toggledata");
+btn.addEventListener("click", function () {
+  if (toggledata.children.length === 0) {
+    const newdiv = document.createElement("div");
+    newdiv.setAttribute("class", "newdivdata");
     
-    }
+    const ul1 = document.createElement("ul");
+    ul1.setAttribute("class", "ul1");
     
-})
-// toggledata.removeChild(newdiv)
-
-
+    const l1 = document.createElement("li");
+    const anchor = document.createElement("a");
+    anchor.innerHTML = "Home";
+    anchor.href = "";
+    l1.appendChild(anchor);
+    
+    const l2 = document.createElement("li");
+    const anchor2 = document.createElement("a");
+    anchor2.innerHTML = "About";
+    anchor2.href = "../About/index.html";
+    l1.appendChild(anchor2);
+    
+    const l3 = document.createElement("li");
+    const anchor3 = document.createElement("a");
+    anchor3.innerHTML = "Contact";
+    anchor3.href = "../Contact/index.html";
+    l1.appendChild(anchor3);
+    
+    ul1.appendChild(l1);
+    ul1.appendChild(l2);
+    ul1.appendChild(l3);
+    newdiv.appendChild(ul1);
+    toggledata.append(newdiv)
+} else if (toggledata.style.display === "none") {
+    toggledata.style.display = "block";
+  } else {
+    toggledata.style.display = "none";
+  }
+});
